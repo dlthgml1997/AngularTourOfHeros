e7870370fe95839790bac7c28f718d112b620b9a
LeeSohee <shfksthgml@naver.com>
2020-03-11 19:00:00 +0900

    서비스생성하기

---
+++ src/app/hero.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class HeroService { 
+
+  constructor(private messageService: MessageService) { } // 서비스에 서비스가 존재하는 경우
+
+  getHeroes(): Observable<Hero[]> {
+    // TODO: 메시지는 히어로 데이터를 가져온 후에 보내야 합니다.
+    this.messageService.add(`HeroService: fetched heroes`);
+    return of(HEROES);
+  }
+
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService} from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -14,12 +16,18 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  heroes = HEROES;
+  heroes : Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+  
+  constructor(private heroService: HeroService) { } // 의존성 주입 시스템이 d인스턴스를 찾아서 전달
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero) : void {
