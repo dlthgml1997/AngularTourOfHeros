62c17252a42ad32cad0413bac713047c7791662b
LeeSohee <shfksthgml@naver.com>
2020-03-12 22:12:51 +0900

    라우팅 및 네비게이션 추가하기

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService} from '../hero.service';
 
 
@@ -9,28 +8,20 @@ import { HeroService} from '../hero.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
-  hero : Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
   heroes : Hero[];
-  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { } // 의존성 주입 시스템이 d인스턴스를 찾아서 전달
 
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
   }
-  
-  constructor(private heroService: HeroService) { } // 의존성 주입 시스템이 d인스턴스를 찾아서 전달
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero) : void {
-    this.selectedHero = hero;
-  }
 }
